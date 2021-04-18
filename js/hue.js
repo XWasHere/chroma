91af48705f72bb01db8e7a866535c4a909baad35
X <70487315+XWasHere@users.noreply.github.com>
2021-04-18 11:25:19 -0400

    Refactor - Remove unused code

--- js/hue.js
+++ js/hue.js
@@ -1,14 +1,6 @@
 const hueInterval = 1;
-const hueMaxChange = 1;
 let rat = document.getElementById("rat");
 let hue = 0;
-
-// random paste from mdn
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-}
 
 function updateRat() {
   rat.style.filter = `hue-rotate(${hue}deg)`;
